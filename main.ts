9bcb258c960ca6498f77b6292a8469d6d079148b
Jdouard <118201220+Jdouard@users.noreply.github.com>
2022-12-08 09:19:03 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let guess = 0
+let value = 12
+let SecondValue = 13
+game.splash("Answer The Question")
+while (guess != value + SecondValue) {
+    guess = parseInt(game.askForString("Sum Of 12 and 13"))
+    if (guess == value + SecondValue) {
+        game.splash("Correct!")
+        game.over(true)
+    } else {
+        game.splash("Incorrect")
+        game.over(false)
+    }
+}
