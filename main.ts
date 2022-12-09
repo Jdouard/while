db63cd3b92f2b0f6a48c8cffb38263c3c86e102e
onmartin <118198239+onmartin@users.noreply.github.com>
2022-12-09 09:58:57 -0700

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 let guess = 0
 scene.setBackgroundColor(11)
 let value = 12
+info.setLife(20)
 let SecondValue = 13
 game.splash("Answer The Question")
 while (guess != value + SecondValue) {
     guess = parseInt(game.askForString("Sum Of 12 and 13"))
     if (guess == value + SecondValue) {
         game.splash("Correct!")
-        game.over(true)
+        info.changeLifeBy(5)
     } else {
         game.splash("Incorrect")
-        game.over(false)
+        info.changeLifeBy(-5)
     }
 }
